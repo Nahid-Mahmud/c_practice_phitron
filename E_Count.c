27a640574c99e375bf8b211d16abf96145eb9d38
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2026-04-23 23:48:00 +0600

    Add program to calculate the sum of digits in a string input

---
+++ E_Count.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char arr[1000001];
+    // fgets(arr, 1000001, stdin);
+    scanf("%s", arr);
+    int sum = 0;
+    int length = strlen(arr);
+
+    for (int i = 0; i < length; i++)
+    {
+        sum = sum + arr[i] - '0';
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
